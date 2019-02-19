312efaa345acec8704fb447e33c18b822bc07a26
huangxiaochang <wb.huangxiaochang@mesg.corp.netease.com>
2019-02-19 17:26:30 +0800

    add linkList

--- data_structures/link_list.js
+++ data_structures/link_list.js
@@ -1,7 +1,7 @@
 import { Comparator } from '../util.js'
 
 class LinkListNode {
-	constructor(value, next) {
+	constructor(value, next=null) {
 		this.value = value
 		this.next = next
 	}
@@ -15,6 +15,34 @@ class linkList {
 		this.head = null
 		this.tail = null
 		this.compare = new Comparator(compareFn)
+	}
+	// prepend a node to a linkList
+	/* @params value *
+		 @retrun linkList
+	*/
+	prepend(value) {
+		const newNode = new LinkListNode(value, this.head)
+		this.head = newNode
+		if (!this.tail) {
+			this.tail = newNode
+		}
+		return this
+	}
+	/*
+		append a new node to a linklist
+		@params value *
+		@retrun linkList
+	*/
+	append(value) {
+		const newNode = new LinkListNode(value)
+		if (!this.head) {
+			this.head = newNode
+			this.tail = newNode
+		} else {
+			this.tail.next = newNode
+			this.tail = newNode
+		}
+		return this
 	}
 }
 
