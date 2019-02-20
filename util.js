7ec5eef5255bad064204cc7ff094b44d2bb8a5e8
huangxiaochang <wb.huangxiaochang@mesg.corp.netease.com>
2019-02-20 19:30:20 +0800

    util

--- util.js
+++ util.js
@@ -1,9 +1,14 @@
-export class Comparator {
+class Comparator {
 	constructor(compareFn) {
 		this.compare = compareFn || Comparator.defaultCompareFn
 	}
-	defaultCompareFn(a, b) {
+	static defaultCompareFn(a, b) {
+		// 静态方法内部的this指向的是类，而不是实例
 		if (a === b) { return 0 }
 		return a < b ? -1 : 1
 	}
+}
+
+module.exports = {
+	Comparator
 }
