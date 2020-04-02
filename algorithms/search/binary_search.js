9bf3555dddc10ade0418f5035e1e2b3dcff61364
huangxiaochang <wb.huangxiaochang@mesg.corp.netease.com>
2020-04-02 13:57:51 +0800

    二分查找某个值的左右边界的索引

--- algorithms/search/binary_search.js
+++ algorithms/search/binary_search.js
@@ -13,6 +13,7 @@ class BinarySearch {
 		this.compareFn = compareFn || (new Comparator(compareFn)).compare
 	}
 
+	// 该二分查找找到一个即返回
 	search(val, start=0, end=this.sortArr.length -1) {
 		while(start <= end) {
 			let middle_pos = Math.floor((start + end) / 2)
@@ -29,6 +30,50 @@ class BinarySearch {
 	}
 }
 
+// 二分查找右边界。如: [1,2,2,2,3],target=2返回3， 即最右边的2
+function searchRightBorder (arr, target) {
+	let l = 0;
+	let r = arr.length;
+	var res = -1
+	// [l, r)左闭右开
+	while (l < r) {
+		let mid = (l + r) >> 1
+		if (arr[mid] === target) {
+			// 找到某个值时，不立即终止查找，而是继续向右二分查找
+			res = mid
+			l = mid + 1
+		} else if (arr[mid] > target) {
+			r = mid
+		} else if (arr[mid] < target) {
+			l = mid + 1
+		}
+	}
+	return res
+}
+
+// 二分查找左边界。如: [1,2,2,2,3],target=2返回3， 即最右边的1
+function searchLefttBorder (arr, target) {
+	let l = 0;
+	let r = arr.length - 1;
+	var res = -1
+	// [l, r]
+	while (l <= r) {
+		let mid = (l + r) >> 1
+		if (arr[mid] === target) {
+			res = mid
+			// 继续先左边二分查找
+			r = mid - 1
+		} else if (arr[mid] > target) {
+			r = mid - 1
+		} else if (arr[mid] < target) {
+			l = mid + 1
+		}
+	}
+	return res
+}
+
 module.exports = {
-	BinarySearch
+	BinarySearch,
+	searchRightBorder,
+	searchLefttBorder
 }
